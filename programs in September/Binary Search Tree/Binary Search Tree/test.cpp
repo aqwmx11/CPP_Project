405cef7b438414d6f12e9aa96d2df31e7bd94f86
illusion <aqwmx11@pku.edu.cn>
2017-09-07 13:20:14 -0400

    add more functions to BST

--- programs in September/Binary Search Tree/Binary Search Tree/test.cpp
+++ programs in September/Binary Search Tree/Binary Search Tree/test.cpp
@@ -18,12 +18,68 @@ void display(myNode* top) {
 	}
 }
 
+//Second function, insert a node into the tree
+void insert(myNode** ptop, int val) {
+	if (*ptop) {
+		if (val < (*ptop)->val)
+			insert(&(*ptop)->left, val);
+		else
+			insert(&(*ptop)->right, val);
+	}
+	else {
+		*ptop = new myNode(val, 0, 0);
+	}
+}
+
+//Third function, delete a BST
+void myDelete(myNode** ptop) {
+	if (*ptop) {
+		if ((*ptop)->left)
+			myDelete(&(*ptop)->left);
+		if ((*ptop)->right)
+			myDelete(&(*ptop)->right);
+		delete *ptop;
+		*ptop = 0;
+	}
+}
+
+//4th function, find the minimum int in a BST
+int findMin(const myNode* top) {
+	if (top == nullptr)
+		return 0;
+	else {
+		for (; top->left; top = top->left);
+		return top->val;
+	}
+}
+
+//5th function, find the sum of left leaves
+//leetCode 404
+//Note that the left leaves means the leaves which have no "lefter" leaves
+int sumOfLeftLeaves(myNode* top) {
+	if (!top)
+		return 0;
+	if (top->left && !top->left->left && !top->left->right)
+		return top->left->val + sumOfLeftLeaves(top->right);
+	return sumOfLeftLeaves(top->left) + sumOfLeftLeaves(top->right);
+}
+
+int findSum(myNode* root) {
+	if (!root)
+		return 0;
+	return findSum(root->left) + findSum(root->right) + root->val;
+}
+
 int main() {
-	myNode a(2);
-	myNode b(1);
-	myNode c(8);
-	myNode d(5, &b, &c);
-	myNode e(3, &a, &d);
-	display(&e);
+	myNode* top = 0;
+	insert(&top, 5);
+	insert(&top, 12);
+	insert(&top, 10);
+	insert(&top, 9);
+	display(top);
+	cout << endl;
+	cout << findMin(top) << endl;
+	cout << findSum(top) << endl;
+	myDelete(&top);
 	return 0;
 }
